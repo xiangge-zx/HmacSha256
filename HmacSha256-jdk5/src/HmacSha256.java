9b9480396ddd3901bf34e441b6dbcbceb3e54049
xuanzhou <xuanzhouc3@163.com>
2024-11-03 21:16:18 +0800

    feat: HmacSha256 jar use jdk8/jdk5

---
+++ HmacSha256-jdk5/src/HmacSha256.java
@@ -0,0 +1,44 @@
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+    public class HmacSha256 {
+        public static String hmacSHA256(String data, String secret) {
+            try {
+                // Create HMAC-SHA256 key from the given secret
+                SecretKeySpec secretKeySpec = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+
+                // Get an instance of Mac object implementing HMAC-SHA256
+                Mac mac = Mac.getInstance("HmacSHA256");
+                mac.init(secretKeySpec);
+
+                // Calculate the HMAC value
+                byte[] hmacBytes = mac.doFinal(data.getBytes());
+
+                // Convert result into a hexadecimal string
+                StringBuilder sb = new StringBuilder(hmacBytes.length * 2);
+                for (byte b : hmacBytes) {
+                    sb.append(String.format("%02x", b));
+                }
+                return sb.toString();
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to calculate HMAC-SHA256", e);
+            }
+        }
+
+        public static void main(String[] args) {
+            if (args.length != 2) {
+                System.out.println("Please provide two integers as arguments.");
+                return;
+            }
+            try {
+                String message = args[0];
+                String secret = args[1];
+
+                String hmacSha256Hex = hmacSHA256(message, secret);
+                System.out.println(hmacSha256Hex);
+            } catch (NumberFormatException e) {
+                System.out.println("The arguments must be integers.");
+            }
+        }
+
+    }
